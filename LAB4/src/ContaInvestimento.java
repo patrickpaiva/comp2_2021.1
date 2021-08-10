93b52596954da9509a570c24e915f88e7085a932
Patrick Paiva <patrickpaiva88@gmail.com>
2021-08-10 14:31:38 -0300

    initial commit

---
+++ LAB4/src/ContaInvestimento.java
@@ -0,0 +1,15 @@
+public class ContaInvestimento extends Conta {
+    private String tipoInvestimento;
+    private float taxaJuros;
+
+    public ContaInvestimento(int numeroDaConta, Correntista correntista) {
+        super(numeroDaConta, correntista);
+    }
+
+    public void aplicarJuros() {
+
+    }
+
+
+
+}
