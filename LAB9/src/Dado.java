86709c371b7a646f557ad0509e6a4eb33a914581
Patrick Paiva <patrickpaiva88@gmail.com>
2021-09-10 10:06:49 -0300

    Interface Sorteador e Classe Dado implementadas

--- LAB9/src/Dado.java
+++ LAB9/src/Dado.java
@@ -1,2 +1,11 @@
-public class Dado {
+import java.util.Random;
+
+public class Dado implements Sorteador {
+    public int sortear() {
+        Random meuRandom = new Random();
+
+        int resultado = meuRandom.nextInt(6);
+
+        return resultado + 1;
+    }
 }
