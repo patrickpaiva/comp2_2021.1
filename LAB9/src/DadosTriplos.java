c3d72b5d2f5fbf03e89e075e0ddc7b2dae619e5a
Patrick Paiva <patrickpaiva88@gmail.com>
2021-09-10 10:13:59 -0300

    Classe DadosTriplos implementada

--- LAB9/src/DadosTriplos.java
+++ LAB9/src/DadosTriplos.java
@@ -1,2 +1,12 @@
-public class DadosTriplos {
+public class DadosTriplos implements Sorteador {
+    @Override
+    public int sortear() {
+        Dado dado = new Dado();
+
+        int resultado1 = dado.sortear();
+        int resultado2 = dado.sortear();
+        int resultado3 = dado.sortear();
+
+        return resultado1 + resultado2 + resultado3;
+    }
 }
