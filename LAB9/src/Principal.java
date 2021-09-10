6b56a1be79c176426f90f047463754b2cad56bcb
Patrick Paiva <patrickpaiva88@gmail.com>
2021-09-10 10:50:55 -0300

    Classe Principal implementada

--- LAB9/src/Principal.java
+++ LAB9/src/Principal.java
@@ -1,2 +1,17 @@
 public class Principal {
+    public static void main(String[] args) {
+        DadosDeGamao dadosDeGamao = new DadosDeGamao();
+        DadosTriplos dadosTriplos = new DadosTriplos();
+
+        JogoMalucoComSorteadores jogo1 = new JogoMalucoComSorteadores(
+                "jogo1",
+                "zezinho",
+                "luizinho",
+                10,
+                dadosDeGamao,
+                dadosTriplos
+        );
+
+        jogo1.jogar();
+    }
 }
