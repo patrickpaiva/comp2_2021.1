c708076f4f4228d5cd924bac79bbfb046288e120
Patrick Paiva <patrickpaiva88@gmail.com>
2021-09-10 10:11:02 -0300

    Classe DadosDeGamao implementada

--- LAB9/src/DadosDeGamao.java
+++ LAB9/src/DadosDeGamao.java
@@ -1,2 +1,12 @@
-public class DadosDeGamao {
+public class DadosDeGamao implements Sorteador {
+    @Override
+    public int sortear() {
+        Dado dado = new Dado();
+        int resultadoDado1 = dado.sortear();
+        int resultadoDado2 = dado.sortear();
+
+        if (resultadoDado1 == resultadoDado2) return resultadoDado1 * 4;
+
+        return resultadoDado1 + resultadoDado2;
+    }
 }
